3ae4234bdad5cbdc99daeedd29137816620f2b6e
EricMorata <ericoensis@gmail.com>
2021-10-21 12:36:49 +0200

    documentos integradpos en el repositorio
    integracion

---
+++ src/paquete/prueba.java
@@ -0,0 +1,12 @@
+package paquete;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola");
+		System.out.println("como estas pichita");
+System.out.println("hola como estas");
+	}
+
+}
